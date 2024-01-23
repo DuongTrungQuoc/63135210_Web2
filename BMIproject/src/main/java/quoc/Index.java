7707c0cac0aca01d0c797e529cb890287cbff51b
dtquoc1202 <quoccccc36@gmail.com>
2024-01-23 13:24:28 +0700

    up

--- BMIproject/src/main/java/quoc/Index.java
+++ BMIproject/src/main/java/quoc/Index.java
@@ -7,10 +7,7 @@ import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import java.io.PrintWriter;
-
-/**
- * Servlet implementation class Index
- */
+@WebServlet("/BMI")
 public class Index extends HttpServlet {
 	private static final long serialVersionUID = 1L;
        
