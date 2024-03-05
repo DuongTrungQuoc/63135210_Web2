06e420ee0479464383535969d2ed0a8ab1a0bc07
dtquoc1202 <quoccccc36@gmail.com>
2024-03-05 16:07:55 +0700

    5/3

---
+++ chaoSpringBoot/src/test/java/vn/dtquoc/chaoSpringBoot/ChaoSpringBootApplicationTests.java
@@ -0,0 +1,13 @@
+package vn.dtquoc.chaoSpringBoot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChaoSpringBootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
